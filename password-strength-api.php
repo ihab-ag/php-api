46512d937ef3b66d5007dd20a76b21fdd6cbde67
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-13 02:03:34 +0300

    password strength api

---
+++ password-strength-api.php
@@ -0,0 +1,32 @@
+<?php
+// get password
+$pass=$_POST['password'];
+// find requirements with regex
+if(!preg_match('@[a-z]@',$pass)){
+    $result=[
+        "result"=>"use at least one lowercase character"
+    ];
+}
+elseif(!preg_match('@[A-Z]@',$pass)){
+    $result=[
+        "result"=>"use at least one uppercase character"
+    ];
+}
+elseif(!preg_match('@[0-9]@',$pass)){
+    $result=[
+        "result"=>"use at least one digit"
+    ];
+}
+elseif(!preg_match('@[\W]@',$pass)){
+    $result=[
+        "result"=>"use at least one special character"
+    ];
+}
+else{
+    $result=[
+        "result"=>"strong password"
+    ];
+}
+// echo results
+echo json_encode($result);
+?>
