4035daadcaa295714086a84d1974985aa9bd6f59
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-13 09:21:02 +0300

    declared equation elements

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ window.onload=()=>{
     const palindromeInput=document.getElementById('palindrome');
     const palindromeBtn=document.getElementById('palindrome-btn');
     const palindromeOutput=document.getElementById('palindrome-output');
+    const a=document.getElementById('a');
+    const b=document.getElementById('b');
+    const c=document.getElementById('c');
+    const solveBtn=document.getElementById('solve-btn');
+    const equationOutput=document.getElementById('equation-output');
 }
