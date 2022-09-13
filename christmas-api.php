f061125fdc5b31bec7438f8e6a5648ca4c275037
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-13 08:33:04 +0300

    christmas api

--- christmas-api.php
+++ christmas-api.php
@@ -1,7 +1,18 @@
 <?php
 // get todays year day
-$today=getdate();
-print_r($today);
-$day=$today[yday]-20;
-echo $day;
+$today = time();
+// get christmas day
+$christmas = strtotime('December 25');
+// diffrence in seconds
+$seconds= $christmas - $today;
+// get days 
+$days= $seconds/86400;
+// round number
+$days = floor($days);
+// results
+$result=[
+    "days"=>$days
+];
+// echo result
+echo json_encode($result);
 ?>
