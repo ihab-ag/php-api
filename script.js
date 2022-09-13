7ef2317fdaa23f88a5de97258cef3401b277d3e4
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-13 09:23:49 +0300

    added days element

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 window.onload=()=>{
     // elements
+    // palindrome
     const palindromeInput=document.getElementById('palindrome');
     const palindromeBtn=document.getElementById('palindrome-btn');
     const palindromeOutput=document.getElementById('palindrome-output');
+    // equation
     const a=document.getElementById('a');
     const b=document.getElementById('b');
     const c=document.getElementById('c');
     const solveBtn=document.getElementById('solve-btn');
     const equationOutput=document.getElementById('equation-output');
+    // pass
     const pass=document.getElementById('pass');
     const passBtn=document.getElementById('pass-btn');
     const passOutput=document.getElementById('pass-output');
+    // days
+    const days=document.getElementById('days');
 }
